9c42030a66079fa5da45733f196ed5d3564fb8e9
yuhyuh703 <matic.bostjancic3@gmail.com>
2025-04-04 13:21:47 +0300

    added delete function

--- src/components/Customers.tsx
+++ src/components/Customers.tsx
@@ -1,8 +1,10 @@
 import {useState, useEffect } from 'react';
-import { AllCommunityModule, ModuleRegistry, ColDef, themeMaterial } from 'ag-grid-community'; 
+import { AllCommunityModule, ModuleRegistry, ColDef, themeMaterial, ICellRendererParams } from 'ag-grid-community'; 
 import { AgGridReact } from 'ag-grid-react'; // React Data Grid Component
 import { Customer } from '../types'; // Customer type
 import AddCustomer from './AddCustomer';
+import Button from '@mui/material/Button';
+import { Snackbar } from '@mui/material';
 
 
 // Register all Community features
@@ -10,6 +12,7 @@ ModuleRegistry.registerModules([AllCommunityModule]);
 
 const Customers = () => {
     const [customers, setCustomers] = useState<Customer[]>([]);
+    const [open, setOpen] = useState(false);
     const [columnDefs] = useState<ColDef<Customer>[]>([
         {field: 'firstname', headerName: 'First Name', sortable: true, filter: true, flex: 1},
         {field: 'lastname', headerName: 'Last Name', sortable: true, filter: true, flex: 1},
@@ -17,7 +20,20 @@ const Customers = () => {
         {field: 'postcode', headerName: 'Postcode', sortable: true, filter: true, flex: 1},
         {field: 'city', headerName: 'City', sortable: true, filter: true, flex: 1},
         {field: 'phone', headerName: 'Phone', sortable: true, filter: true, flex: 1},
-        {field: 'email', headerName: 'Email', sortable: true, filter: true, flex: 1}
+        {field: 'email', headerName: 'Email', sortable: true, filter: true, flex: 1},
+        {
+            width: 120,
+            cellRenderer: (params: ICellRendererParams) => 
+                <Button
+                size="small"
+                variant="outlined"
+                color="error"
+                onClick={() => {handelDelete(params), console.log(params.data)}}
+                >Delete
+                </Button>
+            
+
+        }
     ])
 
 
@@ -41,6 +57,23 @@ const Customers = () => {
     });
     }
 
+    const handelDelete = (params: ICellRendererParams) => {
+        fetch(params.data._links.customer.href, {
+            method: 'DELETE'
+        })
+        .then(response => {
+            if(!response.ok)
+                throw new Error('Failed to fetch');
+            return response.json();
+        })
+        .then(fetchCustomer)
+        .then(() => setOpen(true))
+        .catch(error => {
+            console.error(error);
+        });
+
+    }
+
 
     return (
         <div style={{ width: '100vw', height: '100vh', display: 'flex', flexDirection: 'column' }}>
@@ -55,6 +88,12 @@ const Customers = () => {
                 
                 />
             </div>
+            <Snackbar
+            open={open}
+            autoHideDuration={3000}
+            onClose={() => setOpen(false)}
+            message="Customer deleted"
+            ></Snackbar>
         </div>
     )
 }
